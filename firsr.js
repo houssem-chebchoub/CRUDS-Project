81f884f6809ff334ae3ca34cc5b0e3232b3099c7
Houssem Chebchoub <houssamchebchoub@gmail.com>
2024-09-18 19:41:47 +0100

    we just build the totale function

---
+++ firsr.js
@@ -0,0 +1,53 @@
+let title = document.getElementById('title');
+let price = document.getElementById('price');
+let taxes = document.getElementById('taxes');
+let ads = document.getElementById('ads');
+let discount = document.getElementById('discount');
+let total = document.getElementById('total');
+let count = document.getElementById('count');
+let category = document.getElementById('category');
+let submit = document.getElementById('submit');
+let search = document.getElementById('search');
+let searchTitle = document.getElementById('searchTitle');
+let searchCategory = document.getElementById('searchCategory');
+
+
+
+
+
+
+
+//get total
+
+function getTotal(){
+
+    if( price.value !=''){
+        let result = (+price.value + +taxes.value + +ads.value) - +discount.value;
+        total.innerHTML = result;
+        total.style.backgroundColor ="#040"
+    }else{
+        total.innerHTML = '';
+        total.style.backgroundColor ="#f10";
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+//create product
+//save on the localstorage
+//clear inputes
+//read
+//count
+//delete
+//update
+//search
+//clean data
